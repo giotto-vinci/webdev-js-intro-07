a6ede3e5338eb2a81d40da3ac7421525e09c9014
Roderick Fitzgerald <liber6schol@outlook.com>
2025-01-20 00:40:15 -0500

    I created all functions in assignment.js with correct results rendered in dev tools but still not on live server page. Updated tasks in readme.

--- js/assignment.js
+++ js/assignment.js
@@ -6,3 +6,26 @@ const checkIfEvenElement = document.getElementById("check-if-even");
 const lostNumbersElement = document.getElementById("lost-numbers");
 
 const lostNumbers = [4, 8, 15, 16, 23, 42];
+
+function returnTheRemainder(dividend, devisor) {
+  return dividend % devisor;
+}
+
+console.log(returnTheRemainder(5,4));
+
+function checkIfEven(value) {
+  if (value % 2 === 0) {
+    return true;
+  } else {
+    value % 2 !== 0;
+  return false;
+  }
+}
+
+console.log(checkIfEven(47));
+
+function getTheFourthElement () {
+    return lostNumbers;
+}
+
+console.log(lostNumbers[3]);

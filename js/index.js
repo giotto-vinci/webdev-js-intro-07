ff79cd208c43db29d6d3e8fb272b3ecd56463eb1
Roderick Fitzgerald <liber6schol@outlook.com>
2025-01-19 23:19:54 -0500

    experimented in index.js, with some issues, and updated readme tasks.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,41 @@
 // Prevent us from attempting to use variables
 // that are not declared
-"use strict"
+"use strict";
 
+/*
+function greet(name = "Guest") {
+  console.log("Hello, " + name + "!");
+}
+
+greet();
+greet("Roderick");
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+let sum = add(33, 14);
+console.log(sum);
+
+const square = function (number) {
+    return number * number;
+  };
+  
+  console.log(square(10));
+
+  function book() {
+    console.log("Let's find a good book!");
+  }
+  const title = (book) => {
+    return "This is the great book, " + book + "!";
+}
+
+
+book();
+
+
+book(title);
+
+title("Meditations");
+
+*/
